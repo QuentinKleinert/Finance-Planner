5acbf2f6994fff9ec5b869caa94111e02a9c7df5
Quentin Kleinert <quentinkleinert@Mac-mini.fritz.box>
2024-10-07 17:11:54 +0200

    add time to program

--- finanz-planner/src/App.js
+++ finanz-planner/src/App.js
@@ -1,7 +1,13 @@
+
+import React from 'react';
+import './App.css';
+import DateTime from './components/dateTime';
+
 export default function MyApp() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Welcome User</h1>
+      <DateTime></DateTime>
     </div>
   );
 }
